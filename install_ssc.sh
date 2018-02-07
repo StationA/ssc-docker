6dd4c567f566701b549a6932a4abba7efe9db327
jerluc <jeremyalucas@gmail.com>
2018-02-07 12:05:34 -0800

    [Hotfix] Making install script work for sh

--- install_ssc.sh
+++ install_ssc.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
@@ -9,21 +9,23 @@ SSC_DIR="$BUILD_DIR/ssc-$SSC_VER"
 
 # Download source and unzip contents
 # TODO: Use a proper release when it's available!
-pushd $BUILD_DIR
+OLDPWD=`pwd`
+cd $BUILD_DIR
 wget -O ssc.zip https://github.com/NREL/ssc/archive/$SSC_VER.zip
 unzip ssc.zip
 rm ssc.zip
-popd
+cd $OLDPWD
 
 # Build ssc core libs
-pushd $SSC_DIR/build_linux
+OLDPWD=`pwd`
+cd $SSC_DIR/build_linux
 make -f Makefile-shared -j9
 make -f Makefile-nlopt -j9
 make -f Makefile-lpsolve -j9
 make -f Makefile-solarpilot -j9
 make -f Makefile-tcs -j9
 make -f Makefile-ssc -j9
-popd
+cd $OLDPWD
 
 # Copy built libs to system directories
 mkdir -p $INSTALL_PREFIX/lib $INSTALL_PREFIX/include
