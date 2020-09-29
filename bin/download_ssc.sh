d6b137fb469e25390058916fdc0030978e3c6bf4
Christian Czezatke <christian@stationa.com>
2020-09-28 18:05:31 -0700

    Get rid of include files that hide system includes.

--- bin/download_ssc.sh
+++ bin/download_ssc.sh
@@ -9,5 +9,9 @@ source $BASE_DIR/env.sh
 pushd $ROOT_DIR
 wget -O ssc.zip https://github.com/NREL/ssc/archive/$SSC_VER.zip
 unzip ssc.zip
+
+# Get rid of custom sys/time.h includes that break the build.
+rm -fr $SSC_DIR/nlopt/sys
+
 rm ssc.zip
 popd
